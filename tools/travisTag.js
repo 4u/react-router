886da5ab878425c2d3279481bc86d999e2e5006a
Max Nikitin <maxnik4u@gmail.com>
2017-11-26 05:27:33 +0300

    lint

--- tools/travisTag.js
+++ tools/travisTag.js
@@ -2,7 +2,7 @@ const config = require('../package.json')
 let version = config.version.split('.')
 
 if (process.env.TRAVIS_BUILD_NUMBER) {
-  version[version.length - 1] = process.env.TRAVIS_BUILD_NUMBER;
+  version[version.length - 1] = process.env.TRAVIS_BUILD_NUMBER
 }
 
 process.stdout.write(`v${version.join('.')}`)
